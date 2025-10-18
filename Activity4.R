d0be33797833042b32f4b218aba411060da6923b
omcmorrow <omcmorrow@colgate.edu>
2025-10-18 17:16:09 -0400

    Final Activity 4 code

--- Activity4.R
+++ Activity4.R
@@ -17,16 +17,24 @@ library(tidyverse)
 #3. iris sepal length x petal length
 
 # hint: consider using a list, and also new vectors for regression variables
+
+#Reading in versicolor data
 versicolor <- iris[iris$Species == "versicolor",]
-reg_index <- versicolor(c("Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width"))
-reg_list <- 
-sl_w <- lm(versicolor$Sepal.Length ~ versicolor$Sepal.Width)
-pl_w <- lm(versicolor$Petal.Length ~ versicolor$Petal.Width)
-sl_pl <- lm(versicolor$Sepal.Length ~ versicolor$Petal.Length)
-for(i in 1:3){
-  
+#Creating lists for each regression relationship
+reg_list <- list(
+  c("versicolor$Sepal.Length ~ versicolor$Sepal.Width"),
+  c("versicolor$Petal.Length ~ versicolor$Petal.Width"),
+  c("versicolor$Sepal.Length ~ versicolor$Petal.Length"))
+#Creating output list
+table_list <- list()
+#Regression for loop
+for (i in 1:length(reg_list)) {
+  model <- lm(reg_list[[i]])
+  table_list[[i]] <- summary(model)
 }
-versicolor
+#Reading output
+table_list
+                        
 #####################################
 ##### Part 2: data in dplyr     #####
 #####################################
@@ -36,7 +44,15 @@ versicolor
 height <- data.frame(Species = c("virginica","setosa","versicolor"),
                      Height.cm = c(60,100,11.8))
 
-
+#Installing dplyr package
+install.packages("dplyr")
+library(dplyr)
+#Looking at original iris dataframe
+head(iris)
+#Joining data to a new data frame
+new_iris <- left_join(iris, height, by = "Species")
+#Reading new dataframe
+head(new_iris)
 
 #####################################
 ##### Part 3: plots in ggplot2  #####
@@ -46,13 +62,22 @@ height <- data.frame(Species = c("virginica","setosa","versicolor"),
 plot(iris$Sepal.Length,iris$Sepal.Width)
 
 #3a. now make the same plot in ggplot
+#Installing ggplot package
+install.packages("ggplot2")
+library(ggplot2)
+ggplot(data = iris, aes(x = Sepal.Length, y = Sepal.Width)) + geom_point()
 
-
-#3b. make a scatter plot with ggplot and get rid of  busy grid lines
-
+#3b. make a scatter plot with ggplot and get rid of busy grid lines
+#Using theme_classic to remove grid lines and background
+ggplot(data = iris, aes(x= Sepal.Length, y = Sepal.Width)) + geom_point() + theme_classic()
 
 #3c. make a scatter plot with ggplot, remove grid lines, add a title and axis labels, 
 #    show species by color, and make the point size proportional to petal length
+ggplot(data = iris, aes(x= Sepal.Length, y = Sepal.Width)) + geom_point() + theme_classic()
+  labs(title = "Iris Species Sepal Length vs. Sepal Width",
+       x = "Sepal Length",
+       y = "Sepal Width",
+       size = "Petal Length")
 
 #####################################
 ##### Question: how did         #####
