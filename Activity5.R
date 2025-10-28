c4e71a51d3bbff183c9cf52074389a623ce839d7
omcmorrow <omcmorrow@colgate.edu>
2025-10-28 11:11:37 -0400

    Initial code through question 5

---
+++ Activity5.R
@@ -0,0 +1,58 @@
+###Question 2###
+#Loading lubridate
+library(lubridate)
+#Reading in streamflow data
+datH <- read.csv("Z:/data/hw5_data/stream_flow_data.csv", na.strings = c("Eqp"))
+#Reading column headers
+head(datH)
+#Reading in precipitation data
+datP <- read.csv("Z:/data/hw5_data/2049867.csv")
+#Reading column headers
+head(datP)
+#Subsetting for reliable measurements indicated by USGS data quality
+datD <- datH[datH$discharge.flag == "A",]
+#Converting date and time
+datesD <- as.Date(datD$date, "%m/%d/%Y")
+#Day of year
+datD$doy <- yday(datesD)
+#Calculating year
+datD$year <- year(datesD)
+#Defining the time
+timesD <- hm(datD$time)
+#Defining time for precipitation data
+dateP <- ymd_hm(datP$DATE)
+#Day of year
+datP$doy <- yday(dateP)
+#Calculating year
+datP$year <- year(dateP)
+##Formatting date and time in decimal days/day of year
+#Converting time from string format
+#Decimal hour
+datD$hour <- hour(timesD ) + (minute(timesD )/60)
+#Full decimal time
+datD$decDay <- datD$doy + (datD$hour/24)
+#Calculating decimal year including leap years
+datD$decYear <- ifelse(leap_year(datD$year),datD$year + (datD$decDay/366),
+                       datD$year + (datD$decDay/365))
+#Calculating times for precipitation data
+datP$hour <- hour(dateP ) + (minute(dateP )/60)
+#Full decimal time
+datP$decDay <- datP$doy + (datP$hour/24)
+#Calculating decimal year including leap years
+datP$decYear <- ifelse(leap_year(datP$year),datP$year + (datP$decDay/366),
+                       datP$year + (datP$decDay/365))
+#Reading in data to check date formatting
+head(datD)
+###Question 3###
+#Plotting discharge
+plot(datD$decYear, datD$discharge, type="l", xlab="Year", ylab=expression(paste("Discharge ft"^"3 ","sec"^"-1")))
+#Finding number of observations in streamflow data
+str(datH)
+#Finding number of observations in precipitation data
+str(datP)
+#Finding frequencies of observations
+discharge <- (datD$discharge)
+dis_freq_table <- table(discharge)
+print(dis_freq_table)
+###Question 5###
+
