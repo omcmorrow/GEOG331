c3f53dc787bda3c013e20bca8e81807f2ed57428
omcmorrow <omcmorrow@colgate.edu>
2025-11-11 11:11:05 -0500

    Initial class code

---
+++ ClassCoding11.11.R
@@ -0,0 +1,9 @@
+#Loading in packages
+library(terra)
+library(tidyterra)
+library(FedData)
+#Listing all class files
+f <- list.files("Z:/GEOG331_F25/data/landsat", full.names = T)
+#Opening list
+f
+
