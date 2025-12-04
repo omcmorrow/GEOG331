20917b39385c09215567eecdb46cf303b46445f5
omcmorrow <omcmorrow@colgate.edu>
2025-12-04 11:12:21 -0500

    Code for land cover data

--- ClassCoding11.11.R
+++ ClassCoding11.11.R
@@ -3,7 +3,41 @@ library(terra)
 library(tidyterra)
 library(FedData)
 #Listing all class files
-f <- list.files("Z:/GEOG331_F25/data/landsat", full.names = T)
+f <- list.files("Z:/data/landsat", full.names = T)
 #Opening list
 f
+lc <- rast(f[3:10])
+lc[[5]]
+summary(lc[[5]])
+plot(lc[[5]])
+plot(lc[[5]]*0.0000275-0.2)
+ndvi <- (lc[[5]]-lc[[4]])/(lc[[5]]+lc[[4]])
+names(ndvi) <- "NDVI"
+# create a quick plot to see how the data looks
+plot(ndvi)
+# read in the shape file of dec lands
+dec <- vect("Z:/data/NYS_DEC_Lands/")
 
+# remember from last time that the attribute table just like a data frame
+# we'll use this to subset to Madison County
+mad_dec <- dec[dec$COUNTY=="MADISON",]
+
+# we could also use the crop function
+# what dimensions of our raster layer are used to crop the vector layer
+lc_dec <- crop(dec,lc)
+
+# lastly, create a buffer around the Madison County DEC lands
+# what are the units? 
+mad_buf <- buffer(mad_dec, width = 1000, singlesided = T)
+
+# create a plot to look at our Madison County data
+plot(mad_dec, col = "red")
+plot(mad_buf, col = "yellow", add = T)
+# calculate ndvi for dec lands
+mad_dec$ndvi_in <- zonal(ndvi, mad_dec, fun = "mean")
+
+# calculate ndvi for the buffer outside dec lands
+mad_dec$ndvi_out <- zonal(ndvi, mad_buf, fun = "mean")
+
+# calculate the difference to see if DEC lands are more productive
+mad_dec$ndvi_dif <- mad_dec$ndvi_in-mad_dec$ndvi_out
--- ProjectWork11.20.R
+++ ProjectWork11.20.R
@@ -1,10 +1,32 @@
 ##Plotting land cover of the colorado river basin for 2000
-#Reading in nlcd land cover data for US in 2000
+#Reading in colorado river basin shapefile
+library(sf)
+basin_shp <- st_read("Z:/omcmorrow/Project_Folder/CRB_Outline")
+#Changing the coordinate projection system of the shapefile
+st_crs(basin_shp)
+reproject_basin <- st_transform(basin_shp, crs = "+proj=aea +lat_0=23 +lon_0=-96 +lat_1=29.5 +lat_2=45.5 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs")
+#Reading in land cover data for 2000
 library(terra)
 library(tidyterra)
 library(FedData)
-nlcd_2000 <-get_nlcd(template = FedData::meve,
-                       label = "meve",
-                       year = 2000,
-                       extraction.dir = "Z:/GEOG331_F25/mloranty/data/")
-get_nlcd_annual()
+rast_2000 <- rast("Z:/omcmorrow/Project_Folder/NLCD_Land_Cover_Data/Annual_NLCD_LndCov_2000_CU_C1V1.tif")
+#Changing coordinate systems to match
+crs(rast_2000)
+st_crs(basin_shp)
+basin_shp_proj <- st_transform(basin_shp, crs(rast_2000))
+#Cropping and masking raster to basin shapefile
+crop_rast_2000 <- crop(rast_2000, basin_shp_proj)
+mask_rast_2000 <- mask(crop_rast_2000, basin_shp_proj)
+#Plotting land cover
+plot(mask_rast_2000, main = "Colorado River Basin Land Cover (2000)")
+#Copying the masked raster for reclassification
+copy_filepath <- "Z:/omcmorrow/Project_Folder/NLCD_Land_Cover_Data"
+writeRaster(mask_rast_2000, filename = copy_filepath, format = "GTiff", overwrite = TRUE)
+#Reclassifying raster classes to agriculture and non agriculture
+class_counts <- freq(mask_rast_2000)
+print(class_counts)
+rcl_matrix <- matrix(c(
+  0, 12, 1,
+  15, 16, 1,
+  13, 14, 2
+), ncol = 3, byrow = TRUE)
